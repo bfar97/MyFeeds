4a0a412d3e33a5e1ece1a57ffe7d758a819bf3ba
Bernardo Rodrigues <bernardinhofilipe@hotmail.com>
2015-08-30 18:44:43 +0100

    main design finished

--- js/main.js
+++ js/main.js
@@ -1,26 +1,8 @@
 var app = angular.module('mainApp', ['ngMaterial']);
 
-	app.controller('sidemenuCtrl', ['$scope', '$timeout', '$mdSidenav', '$mdUtil', '$log', 
-		function($scope, $timeout, $mdSidenav, $mdUtil, $log){
-			$scope.toggleleft = buildToggler('left');
-			function buildToggler (navID) {
-				var debounceFn = $mdUtil.debounce(function () {
-					$mdSidenav(navID)
-						.toggle()
-						.then(function (){
-							$log.debug("toggle" + navID + "is done.");
-						});
-				}, 200);
-				return debounceFn;
+	app.controller('sidemenuCtrl', 
+		function($scope, $mdSidenav){
+			$scope.toggleLeft = function () {
+				$mdSidenav('left').toggle();
 			}
-	}]);
-
-	app.controller('leftCtrl', ['$scope', '$timeout', '$mdSidenav', '$mdUtil', '$log', 
-		function($scope, $timeout, $mdSidenav,$mdUtil, $log){
-			$scope.close = function () {
-				$mdSidenav('left').close()
-				.then(function () {
-					$log.debug("close left is done.");
-				});
-			};
-		}]);
+	});
